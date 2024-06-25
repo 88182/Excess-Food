145759d3c22cc74679bb7dececc6b3a22397aa31
Rakshitha  H D <109800487+88182@users.noreply.github.com>
2024-06-25 14:59:42 +0530

    Add files via upload

---
+++ static/js/ajax.js
@@ -0,0 +1,65 @@
+function addFood() {
+  $("#heading").text("Add Food");
+  $("#addFoodModal").modal("show");
+}
+
+function editProfile() {
+  $("#heading").text("Edit Profile");
+  $("#editProfileModal").modal("show");
+}
+
+function editProfile1() {
+  $("#heading").text("Edit Food");
+  $("#editFoodModal").modal("show");
+}
+
+function editFood(id) {  
+  $.ajax({
+    type: "GET",
+    url: "/get_food_details/",
+    data: {
+      id: id,
+    },
+    dataType: "json",
+    success: function (data) {
+      console.log("Food Details:", data);
+
+      // Assuming you have a form with the following input fields
+      $("#id").val(data.id);
+      $("#name").val(data.name);
+      $("#type").val(data.type);
+      $("#preparation_time").val(data.prepared_time);
+      $("#is_del").val(data.is_deliverable);
+      // $("#ingredients").val(data.ingredients);
+      $("#qty").val(data.quantity);
+      $("#description").val(data.description);
+      $("#images").val(data.image);
+
+      // if (data.ingredients && data.ingredients.length > 0) {
+      //   // Iterate through the array and check the corresponding checkboxes
+      //   for (var i = 0; i < data.ingredients.length; i++) {
+      //     var ingredientValue = data.ingredients[i];
+      //     // Check the checkbox with the corresponding value
+      //     $("input[name='ingredients'][value='" + ingredientValue + "']").prop("checked", true);
+      //   }
+      // }
+
+
+      // $("#foodImage").attr("src", data.image);
+
+      $("#editFoodModal").modal("show");
+    },
+    error: function (error) {
+      console.log("Error:", error);
+    },
+  });
+}
+
+  function orderNow() {
+    $("#heading").text("Order Food");
+    $("#orderFoodModal").modal("show");
+  }
+
+  function requestNow() {
+    $("#requestModal").modal("show");
+  }
---
+++ static/js/userValidate.js
@@ -0,0 +1,284 @@
+$(document).ready(function () {
+  $("#userSignup").submit(function (event) {
+      event.preventDefault();
+      if (validateBus()) {
+          this.submit();
+      }
+  });
+
+  const showError = (message) => {
+      const errorDisplay = $(".error");
+      errorDisplay.text(message);
+      errorDisplay
+          .closest(".input-control")
+          .addClass("error")
+          .removeClass("success");
+  };
+
+  const showSuccess = () => {
+      const errorDisplay = $(".error");
+      errorDisplay.text("");
+      errorDisplay
+          .closest(".input-control")
+          .addClass("success")
+          .removeClass("error");
+  };
+
+  const validateBus = () => {
+      const name = $("#name");
+      const email = $("#email");
+      const phone = $("#phone");
+      const gender = $("#gender");
+      const address = $("#address");
+      const location = $("#location");
+      const password = $("#password");
+      const repassword = $("#repassword");
+
+      if (name.val() == "") {
+          showError("Name is required");
+          return false;
+      }
+      if (email.val() === "") {
+          // Assuming '-1' is the default/invalid option
+          showError("Email is required");
+          return false;
+      }
+      if (phone.val() == "") {
+          showError("Phone is required");
+          return false;
+      }
+      if (gender.val() == "-1") {
+          showError("Gender is required");
+          return false;
+      }
+      if (address.val() == "") {
+          showError("Address is required");
+          return false;
+      }
+      if (location.val() == "-1") {
+        showError("Location is required");
+        return false;
+    }
+      if (password.val() == "") {
+        showError("Password is required");
+        return false;
+      }
+      if (repassword.val() == "") {
+          showError("Password is required");
+          return false;
+      }
+      showSuccess();
+      return true;
+  };
+});
+
+$(document).ready(function () {
+  $("#ratingForm").submit(function (event) {
+      event.preventDefault();
+      if (validateBus()) {
+          this.submit();
+      }
+  });
+
+  const showError = (message) => {
+      const errorDisplay = $(".error");
+      errorDisplay.text(message);
+      errorDisplay
+          .closest(".input-control")
+          .addClass("error")
+          .removeClass("success");
+  };
+
+  const showSuccess = () => {
+      const errorDisplay = $(".error");
+      errorDisplay.text("");
+      errorDisplay
+          .closest(".input-control")
+          .addClass("success")
+          .removeClass("error");
+  };
+
+  const validateBus = () => {
+      const order_no = $("#order_id");
+      const rate = $("#ratings");
+      const description = $("#description");
+
+      if (order_no.val() === "-1") {
+          showError("Select food on order_id is required");
+          return false;
+      }
+      if (rate.val() === "-1") {
+          showError("Ratings is required");
+          return false;
+      }
+      if (description.val() == "") {
+          showError("Description is required");
+          return false;
+      }
+      showSuccess();
+      return true;
+  };
+});
+
+
+
+$(document).ready(function () {
+    $("#orderNow").submit(function (event) {
+        event.preventDefault();
+        if (validateBus()) {
+            this.submit();
+        }
+    });
+  
+    const showError = (message) => {
+        const errorDisplay = $(".error");
+        errorDisplay.text(message);
+        errorDisplay
+            .closest(".input-control")
+            .addClass("error")
+            .removeClass("success");
+    };
+  
+    const showSuccess = () => {
+        const errorDisplay = $(".error");
+        errorDisplay.text("");
+        errorDisplay
+            .closest(".input-control")
+            .addClass("success")
+            .removeClass("error");
+    };
+  
+    const validateBus = () => {
+        const qty = $("#quantity");
+        const order_no = $("#address");
+        const description = $("#description");
+
+        if (qty.val() === "") {
+            showError("Quantity is required");
+            return false;
+        }
+        if (parseInt(qty.val()) <= 0) {
+            showError("Quantity must be greater than 0");
+            return false;
+        }
+    
+        if (order_no.val() === "") {
+            showError("Address is required");
+            return false;
+        }
+        
+        
+        if (description.val() === "") {
+            showError("Description is required");
+            return false;
+        }
+        showSuccess();
+        return true;
+    };
+    
+  });
+
+
+
+
+$(document).ready(function () {
+    // add new bus
+    $("#requestForm").submit(function (event) {
+        event.preventDefault();
+        if (validateBus()) {
+            // var formData = new FormData(this);
+            this.submit();
+        }
+    });
+
+    const showError = (message) => {
+        const errorDisplay = $(".error");
+        errorDisplay.text(message);
+        errorDisplay
+            .closest(".input-control")
+            .addClass("error")
+            .removeClass("success");
+    };
+
+    const showSuccess = () => {
+        const errorDisplay = $(".error");
+        errorDisplay.text("");
+        errorDisplay
+            .closest(".input-control")
+            .addClass("success")
+            .removeClass("error");
+    };
+
+    const validateBus = () => {
+        const foodName = $("#food_name");
+        const qty = $("#qty");
+        const desc = $("#description");
+        const SelectType = $("#type");
+        const req_date = $("#req_date");
+
+        if (foodName.val() == "-1") {
+            showError("Food name is required");
+            return false;
+        }
+        if (SelectType.val() === "-1") {
+            // Assuming '-1' is the default/invalid option
+            showError("Please select a valid option");
+            return false;
+        }
+        if (qty.val() == "") {
+            showError("Quantity is required");
+            return false;
+        }
+        if (req_date.val() == "") {
+            showError("Date is required");
+            return false;
+        }
+        if (desc.val() == "") {
+            showError("Description is required");
+            return false;
+        } 
+
+        showSuccess();
+        return true;
+    };
+});
+
+$(document).ready(function () {
+    // add new bus
+    $("#checkQR").submit(function (event) {
+        event.preventDefault();
+        if (validateBus()) {
+            // var formData = new FormData(this);
+            this.submit();
+        }
+    });
+
+    const showError = (message) => {
+        const errorDisplay = $(".error");
+        errorDisplay.text(message);
+        errorDisplay
+            .closest(".input-control")
+            .addClass("error")
+            .removeClass("success");
+    };
+
+    const showSuccess = () => {
+        const errorDisplay = $(".error");
+        errorDisplay.text("");
+        errorDisplay
+            .closest(".input-control")
+            .addClass("success")
+            .removeClass("error");
+    };
+
+    const validateBus = () => {
+        const img = $("#image");
+
+        if (img[0].files.length == 0) {
+            showError("QR code is required");
+            return false;
+        }
+        showSuccess();
+        return true;
+    };
+});
